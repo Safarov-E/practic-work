de1735c7286d8b5b76dca2669a21e03347959e86
Safarov <elkhan.safarov10@yandex.com>
2020-05-15 13:57:31 +0500

    отправка данных из контактной формы

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -142,4 +142,41 @@ window.addEventListener('DOMContentLoaded', function() {
             input[i].value = '';
         }
     })
+
+
+    let formContact = document.querySelector('#form');
+    
+    formContact.addEventListener('submit', function(event) {
+        event.preventDefault();
+        formContact.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        let formData = new FormData(formContact);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        })
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if(request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status === 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        })
+
+        for(let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    })
 })
