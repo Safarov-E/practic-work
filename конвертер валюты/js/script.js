49e20933cf7cd907cbf7865d4972d6c1c2cce91d
Safarov <elkhan.safarov10@yandex.com>
2020-05-21 13:48:21 +0500

    Переписать скрипт конвертер валют, используя промисы.

--- конвертер валюты/js/script.js
+++ конвертер валюты/js/script.js
@@ -2,18 +2,27 @@ let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
 inputRub.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
-
-    request.addEventListener('readystatechange', function(){
-        if(request.readyState === 4 && request.status == 200) {
+    function getConverter() {
+        return new Promise((resolve, reject) => {
+            let request = new XMLHttpRequest();
+            request.open('GET', 'js/current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+        
+            request.addEventListener('readystatechange', function(){
+                if(request.readyState === 4 && request.status == 200) {
+                    resolve();
+                } else {
+                    reject();
+                }
+            }) 
+        })
+    }
+    getConverter()
+        .then(() => {
             let data = JSON.parse(request.response);
             inputUsd.value = inputRub.value/ data.usd;
             inputUsd.value = Number(inputUsd.value).toFixed(4)
-        } else {
-            inputUsd.value = "Что-то пошло не так!";
-        }
-    }) 
+        })
+        .catch(() => inputUsd.value = "Что-то пошло не так!");
 })
