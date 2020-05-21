61706d4e201e291a9110ab7172db741f26c582d4
Safarov <elkhan.safarov10@yandex.com>
2020-05-21 13:55:34 +0500

    Переписать скрипт конвертер валют API, используя промисы.

--- конвертер валюты по API/js/xhr.js
+++ конвертер валюты по API/js/xhr.js
@@ -6,24 +6,32 @@ let inputRub = document.getElementById('rub'),
     course = document.querySelector('.Course'),
     payment = document.querySelector('.Payment'),
     span = document.querySelector('span');
-    
-var request = new XMLHttpRequest;
-request.overrideMimeType("application/json");
-request.addEventListener('readystatechange', function(){
-    if(request.readyState === 4 && request.status == 200) {
-        let valute = JSON.parse(request.response);
-        for(let key in valute.Valute) {
-            optionTag.innerHTML += `<option>${valute.Valute[key].Name}</option>`
-            buttonTag.addEventListener('click', function() {
-                if(optionTag.value === valute.Valute[key].Name) {
-                    currency.textContent = valute.Valute[key].Name;
-                    course.textContent = valute.Valute[key].Value;
-                    payment.textContent = inputRub.value / valute.Valute[key].Value;
-                    span.textContent = valute.Valute[key].CharCode;
+  
+function getConverter() {
+    return new Promise((resolve, reject) => {
+        var request = new XMLHttpRequest;
+        request.overrideMimeType("application/json");
+        request.addEventListener('readystatechange', function(){
+            if(request.readyState === 4 && request.status == 200) {
+                let valute = JSON.parse(request.response);
+                for(let key in valute.Valute) {
+                    optionTag.innerHTML += `<option>${valute.Valute[key].Name}</option>`
+                    buttonTag.addEventListener('click', function() {
+                        if(optionTag.value === valute.Valute[key].Name) {
+                            resolve();
+                        }
+                    })
                 }
-            })
-        }
-    }
-});
-request.open("GET", 'https://www.cbr-xml-daily.ru/daily_json.js', !0);
-request.send()
+            }
+        });
+        request.open("GET", 'https://www.cbr-xml-daily.ru/daily_json.js', !0);
+        request.send()
+    })
+}
+getConverter()
+    .then(() => {
+        currency.textContent = valute.Valute[key].Name;
+        course.textContent = valute.Valute[key].Value;
+        payment.textContent = inputRub.value / valute.Valute[key].Value;
+        span.textContent = valute.Valute[key].CharCode;
+    })
